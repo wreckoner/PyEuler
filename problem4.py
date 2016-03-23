fc4c23f2ac1535dcaef764ab14383ef9e4bdbdb8
Dibyendu Das <dibyendudas.1990@gmail.com>
2016-03-23 10:44:48 -0400

    + Problem5 solution

--- problem4.py
+++ problem4.py
@@ -4,9 +4,12 @@ Problem 4
 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
+
+Answer: 906609
 '''
 
 def is_palindrome(num):
+	""" Returns true if a number is a palindrome """
 	reverse, forward = 0, num
 	while (num > 0):
 		reverse = reverse*10 + num%10
@@ -14,12 +17,13 @@ def is_palindrome(num):
 	return forward == reverse
 
 def main():
-	maximum = 999 * 999
-	while maximum > 100 * 100:
-		maximum -= 1
-		if is_palindrome(maximum):
-			return is_product(maximum)
-	return False
+	upper, lower = 999, 100
+	largest = 0
+	for a in xrange(upper, lower, -1):
+		for b in xrange(a, lower, -1):
+			if is_palindrome(a*b) and largest < a*b:
+				largest = a*b
+	return largest
 
 if __name__ == "__main__":
-	main()
+	print main()
---
+++ problem5.py
@@ -0,0 +1,31 @@
+"""
+Problem 5
+
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+Answer: 232792560
+"""
+
+def main(upper):
+	"""
+	This method returns the smallest multiple of all the integes less than or equal to <upper>.
+	It has an upper bound of x^2 on the time complexity. 
+	"""
+	numbers = range(upper+1)
+	lcm = 1	#lowest common multiple.
+	for index, number in enumerate(numbers):
+		print numbers
+		if index <= 1:
+			continue
+		else:
+			for j in xrange(index+1, upper+1):
+				if numbers[j]%number == 0:
+					numbers[j] /= number
+			lcm *= number
+	return lcm
+
+
+if __name__ == "__main__":
+	print main(20)
