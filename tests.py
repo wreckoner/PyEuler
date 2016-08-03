aaff85d83504ddf7b44e16f03ca90e38911a7595
Dibyendu Das <dibyendudas.1990@gmail.com>
2016-08-03 13:58:35 -0400

    + tests for 23, 24 and 25

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@
 # -*-coding:utf-8 -*-
 
 import unittest
-from src import problem001, problem002, problem003, problem004, problem005, problem006, problem007, problem008, problem009, problem010, problem011, problem012, problem013, problem014, problem015, problem016, problem017, problem018, problem019, problem020, problem021, problem022, problem023
+from src import problem001, problem002, problem003, problem004, problem005, problem006, problem007, problem008, problem009, problem010, problem011, problem012, problem013, problem014, problem015, problem016, problem017, problem018, problem019, problem020, problem021, problem022, problem023, problem024, problem025
 
 class Test_Problems(unittest.TestCase):
 
@@ -72,6 +72,15 @@ class Test_Problems(unittest.TestCase):
 
 	def test_problem_22(self):
 		self.assertEqual(problem022.names_scores("src/files/p022_names.txt"), 871198282)
+
+	def test_problem_23(self):
+		self.assertEqual(problem023.non_abundant_sums(28123), 4179871)
+
+	def test_problem_24(self):
+		self.assertEqual(problem024.lexicographic_permutations(), 2783915460)
+
+	def test_problem_25(self):
+		self.assertEqual(problem025.fibonacci(1000), 4782)
 
 
 
